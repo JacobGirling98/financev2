b6c037885d936e6bb0ebc265062567682c766527
JacobGirling98 <jacob.girling98@gmail.com>
2021-11-08 22:16:38 +0000

    Small layout change for view money

--- frontend/src/views/ViewMoney/ViewMoney.tsx
+++ frontend/src/views/ViewMoney/ViewMoney.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const ViewMoney: React.FC = () => {
   return (
     <>
+      <div className="px-3">
+        <h1 className="display-4 text-center">
+          View Money
+        </h1>
+      </div>
       <div className="mx-auto w-75">
         <div className="row g-3">
+          <div className="col-md">
+            <div className="card p-3">
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
           <div className="col-md">
             <div className="card p-3">
               <div className="card-body">
